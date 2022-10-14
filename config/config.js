1d4f2500303a30f49a5b1191abe86639b0366116
Amin Tehrani <ahosse19@my.centennialcollege.ca>
2022-10-14 15:44:35 -0700

    Added Mongo Atlas Configuration

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI  = "mongodb://127.0.0.1/media"
+//export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://amin:mSRinPogjfKjfPYo@comp229-f2022amin.dfozdf8.mongodb.net/media1?retryWrites=true&w=majority"
